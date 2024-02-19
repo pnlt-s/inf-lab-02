a16096cd0dc26a990d3c69baa65904b4356f5ede
pnlt-s <painalternates@gmail.com>
2024-02-19 15:07:09 +0300

    :)

---
+++ funny.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+touch new
+mv new old
+echo "uwu" > old
+rm old
+mkdir owo
+cd owo
+uname -a > file
+cp file file2
+cat file2 > file3
+chmod +x file3
+ps > processes
+chown uwu:users processes
+ls > files
+tar -c . -f ../archive.tar.gz
+cd ../
+tar -x -f archive.tar.gz -d archive
+rm -rf owo
+
